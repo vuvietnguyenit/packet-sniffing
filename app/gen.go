7debedc53ba093fc44941ad14d4078738aabdc2b
vunv <vunv@coccoc.com>
2025-10-14 14:07:39 +0700

    add arch build

--- app/gen.go
+++ app/gen.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -output-dir ./internal/ebpf -go-package ebpf -output-suffix _gobpf -tags linux mysqlTrace ./bpf/mysql_tcp_trace.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -output-dir ./internal/ebpf -go-package ebpf -output-suffix _gobpf -tags linux mysqlResponseTrace ./bpf/mysql_response_trace.c -- -I./headers -D__TARGET_ARCH_x86
