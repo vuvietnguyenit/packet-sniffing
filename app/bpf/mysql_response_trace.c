8a78f32ee687c2825e5c6568378f3cad3a9b84e8
vunv <vunv@coccoc.com>
2025-10-15 13:44:07 +0700

    SRE-105: filter only ERR_PACKET and send it to rb

--- app/bpf/mysql_response_trace.c
+++ app/bpf/mysql_response_trace.c
@@ -5,33 +5,66 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
+#define MYSQL_ERRMSG_SIZE                                                      \
+  512 // we need to ensure that msg data of MySQL protocol does not exceed this
+      // size
+#define PORT_FILTER 3306
+#define ERR_BUF_SIZE 32 // only need small amount to check for 0xFF
+
 struct data_t {
   __u32 saddr;
   __u32 daddr;
   __u16 sport;
   __u16 dport;
   size_t size;
-  char msg[400];
+  char msg[MYSQL_ERRMSG_SIZE];
 };
 
 struct {
-  __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
-  __uint(key_size, sizeof(__u32));
-  __uint(value_size, sizeof(__u32));
-  __uint(max_entries, 1024); // required on older kernels
+  __uint(type, BPF_MAP_TYPE_RINGBUF);
+  __uint(max_entries, 1 << 24); // 16 MB buffer
 } events SEC(".maps");
 
 // Define your port filter here or set dynamically
-#define PORT_FILTER 3306
+
+static __always_inline void send_mysql_event(__u32 saddr, __u32 daddr,
+                                             __u16 sport, __u16 dport,
+                                             const struct iovec *pack,
+                                             size_t size) {
+  if (size > MYSQL_ERRMSG_SIZE) {
+    bpf_printk("Warning: size %d exceeds MYSQL_ERRMSG_SIZE, truncating\n",
+               size);
+    size = MYSQL_ERRMSG_SIZE;
+  }
+
+  struct data_t *e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
+  if (!e)
+    return;
+
+  e->saddr = saddr;
+  e->daddr = daddr;
+  e->sport = sport;
+  e->dport = dport;
+  e->size = size;
+  bpf_probe_read_user(e->msg, size, pack);
+  bpf_ringbuf_submit(e, 0);
+}
+
+// Return 1 if the buffer contains 0xFF, else 0
+static __always_inline int contains_ff(const unsigned char *data) {
+  for (int i = 0; i < sizeof(data); i++) {
+    if (data[i] == 0xff)
+      return 1;
+  }
+  return 0;
+}
 
 SEC("kprobe/tcp_sendmsg")
 int BPF_KPROBE(tcp_sendmsg, struct sock *sk, struct msghdr *msg, size_t size) {
   struct data_t data = {};
   struct iov_iter iter = {};
   const struct iovec *iovp = NULL;
-  struct iovec iov = {};
-  void *base = NULL;
-  unsigned char buf[32]; // dump first 32 bytes
+  unsigned char mysql_err_buf_check[ERR_BUF_SIZE];
 
   // Read socket info (CO-RE safe)
   data.sport = BPF_CORE_READ(sk, __sk_common.skc_num);
@@ -39,26 +72,24 @@ int BPF_KPROBE(tcp_sendmsg, struct sock *sk, struct msghdr *msg, size_t size) {
   if (data.sport != PORT_FILTER)
     return 0;
 
-  bpf_printk("Hello im here");
   data.saddr = BPF_CORE_READ(sk, __sk_common.skc_rcv_saddr);
   data.daddr = BPF_CORE_READ(sk, __sk_common.skc_daddr);
   data.size = size;
 
   // Read the iov_iter structure from msg->msg_iter
   bpf_core_read(&iter, sizeof(iter), &msg->msg_iter);
-  bpf_printk("iter type: %d\n", iter.iter_type);
-
   // Try to read __iov pointer inside iov_iter
   bpf_core_read(&iovp, sizeof(*iovp), &iter.__iov);
   if (!iovp)
     return 0;
 
-  bpf_printk("iovp string: %s\n", iovp);
-  bpf_probe_read_user(buf, sizeof(buf), iovp);
-  for (int i = 0; i < 32; i++) {
-    bpf_printk("%02x ", buf[i]);
+  bpf_probe_read_user(mysql_err_buf_check, sizeof(mysql_err_buf_check), iovp);
+
+  if (contains_ff(mysql_err_buf_check)) {
+
+    send_mysql_event(data.saddr, data.daddr, data.sport, data.dport, iovp,
+                     data.size);
   }
-  bpf_printk("\n");
   return 0;
 }
 
